32e8114d1bbd9e005e58a319693f3350ebb6b653
Lenisha Murkar <murkar033@gmail.com>
2025-07-07 13:52:03 +0530

    Initial commit: React + Tailwind setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div className="text-3xl font-bold underline text-blue-600">
+        PhysioThreaphy
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
