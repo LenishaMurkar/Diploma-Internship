66b6a6c2db4286eb58783ffa0b60e7750b9ce5f9
Lenisha Murkar <murkar033@gmail.com>
2025-07-08 21:18:04 +0530

    Prototype whatsapp float

--- src/components/WhatsappButton.js
+++ src/components/WhatsappButton.js
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/919876543210?text=Hi%2C%20I%20would%20like%20to%20make%20an%20enquiry%20about%20PhysioCare%20home%20services"
+      href="https://wa.me/919607460621?text=Hi%2C%20I%20would%20like%20to%20make%20an%20enquiry%20about%20PhysioCare%20home%20services"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-5 right-5 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 z-50"
